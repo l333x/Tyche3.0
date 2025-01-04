2c555b74148d74f08b585937d0de83199f74e7e8
l333x <158372793+l333x@users.noreply.github.com>
2025-01-04 11:14:52 -0500

    Update iniciosistema.js
    creación nuevos usuarios

--- js/iniciosistema.js
+++ js/iniciosistema.js
@@ -12,7 +12,31 @@ const usuarios = [
     { cedula: "002", nombre: "Gabriel", apellidos: "Toaquiza", carrera: "Negocios Digitales" },
     { cedula: "003", nombre: "Josue", apellidos: "Martinez", carrera: "Negocios Digitales" },
     { cedula: "004", nombre: "Sebastian", apellidos: "Moreno", carrera: "Negocios Digitales" },
-    { cedula: "005", nombre: "Mateo", apellidos: "Carcelen", carrera: "Negocios Digitales" }
+    { cedula: "005", nombre: "Mateo", apellidos: "Carcelen", carrera: "Negocios Digitales" },
+    { cedula: "1722273487", nombre: "Joseph", apellidos: "Torres", carrera: "Psicología" },
+    { cedula: "1756066088", nombre: "Alan", apellidos: "Singaña", carrera: "Negocios Digitales" },
+    { cedula: "1727522490", nombre: "Daniel", apellidos: "Tituaña", carrera: "Negocios Digitales" },
+    { cedula: "0820226230", nombre: "Mateo", apellidos: "Yanez", carrera: "Negocios Digitales" },
+    { cedula: "0114368753", nombre: "Anahi", apellidos: "Díaz", carrera: "Negocios Digitales" },
+    { cedula: "1823456706", nombre: "Pamela", apellidos: "Padilla", carrera: "Negocios Digitales" },
+    { cedula: "1246789058", nombre: "Jesús", apellidos: "Barreto", carrera: "Negocios Digitales" },
+    { cedula: "1720203650", nombre: "Diana", apellidos: "Carrera", carrera: "Negocios Digitales" },
+    { cedula: "1726251134", nombre: "Joffre", apellidos: "Chacón", carrera: "Negocios Digitales" },
+    { cedula: "1759696492", nombre: "Erick", apellidos: "Carvajal", carrera: "Odontología" },
+    { cedula: "1722654405", nombre: "Amelia", apellidos: "Reinoso", carrera: "Negocios Digitales" },
+    { cedula: "1036839165", nombre: "Xavier", apellidos: "Casa", carrera: "Negocios Digitales" },
+    { cedula: "1839250147", nombre: "Jorge", apellidos: "Rodríguez", carrera: "Negocios Digitales" },
+    { cedula: "1725254895", nombre: "Camila", apellidos: "Andrade", carrera: "Negocios Digitales" },
+    { cedula: "1162374900", nombre: "Diego", apellidos: "Benítez", carrera: "Negocios Digitales" },
+    { cedula: "1755436415", nombre: "Emily", apellidos: "Romero", carrera: "Psicología" },
+    { cedula: "1725083297", nombre: "Carlos", apellidos: "Rodriguez", carrera: "Psicología" },
+    { cedula: "1753461092", nombre: "Christian", apellidos: "Mármol", carrera: "Derecho" },
+    { cedula: "1729165942", nombre: "Emily", apellidos: "Toledo", carrera: "Psicología" },
+    { cedula: "1753717667", nombre: "Alexey", apellidos: "Rubio", carrera: "Psicología" },
+    { cedula: "1751284678", nombre: "Daniela", apellidos: "Lema", carrera: "Derecho" },
+    { cedula: "1750777938", nombre: "Patricio", apellidos: "Simba", carrera: "Derecho" },
+    { cedula: "1106076878", nombre: "Emily", apellidos: "Marín", carrera: "Derecho" },
+    { cedula: "1725499691", nombre: "Seleni", apellidos: "Jurado", carrera: "Psicología" }
 ];
 
 // Abrir y cerrar modales
@@ -102,7 +126,7 @@ document.getElementById("loginForm").addEventListener("submit", function (e) {
                 window.location.href = "registro.html";
                 break;
             default:
-                alert("No se pudo redirigir.");
+                
         }
     } else {
         alert("Cédula incorrecta. Intenta de nuevo.");
