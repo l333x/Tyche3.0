1bd3bd13a5fb428b6950022dfbb046bba1efb847
l333x <158372793+l333x@users.noreply.github.com>
2025-01-12 11:01:49 -0500

    Update politicas.js
    Actualizado 4.0

--- js/politicas.js
+++ js/politicas.js
@@ -1,17 +1,16 @@
-// Seleccionar elementos
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const navLinks = document.querySelector(".nav-links");
-const logoutButton = document.getElementById("logoutButton");
+// Selección del menú de hamburguesa y los enlaces
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navLinks = document.querySelector('.nav-links');
 
-// Alternar visibilidad del menú en pantallas pequeñas
-hamburgerMenu.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+// Alternar visibilidad del menú de navegación
+hamburgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
 });
 
 // Cerrar el menú de hamburguesa al hacer clic en un enlace
-document.querySelectorAll(".nav-links a").forEach(link => {
-    link.addEventListener("click", () => {
-        navLinks.classList.remove("active");
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.classList.remove('active');
     });
 });
 
